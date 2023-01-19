85c059ca6d5cc05fd577df77e47ff894e6dac155
originalkamaal <56730770+originalkamaal@users.noreply.github.com>
2023-01-19 11:18:53 +0530

    4 files modified, 8 added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Navbar from '../components/navbar';
+import { AuthContextProvider } from '../context/AuthContext';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <AuthContextProvider>
+      <Navbar>
+        <Component {...pageProps} />
+      </Navbar>
+    </AuthContextProvider>
+  );
 }
+
+export default MyApp;
