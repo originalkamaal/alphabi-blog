6e467a470080c4e37974c7cc11207be1d407229f
Ahmed Kamal F Khan <Originalkamaal@gmail.com>
2023-01-21 16:37:20 +0530

    updated ArticleFeatured

--- src/components/ArticlesFeatured.tsx
+++ src/components/ArticlesFeatured.tsx
@@ -3,6 +3,40 @@ import { Flex, Grid, GridItem } from "@chakra-ui/layout";
 import React from "react";
 
 const ArticleFeatured = () => {
+  const featured = [
+    {
+      image:
+        "https://demo.tagdiv.com/newspaper_pro/wp-content/uploads/2019/08/h104-696x464.jpg",
+      cat: "fashion",
+      title: "Sample Title for First Article from Featured Articles",
+      author: "Kamaal Khan",
+      published_on: 1674298614,
+    },
+    {
+      image:
+        "https://demo.tagdiv.com/newspaper_pro/wp-content/uploads/2019/08/28-696x368.jpg",
+      cat: "fashion",
+      title: "Sample Title for First Article from Featured Articles",
+      author: "Kamaal Khan",
+      published_on: 1674298614,
+    },
+    {
+      image:
+        "https://demo.tagdiv.com/newspaper_pro/wp-content/uploads/2019/08/54-1-696x405.jpg",
+      cat: "fashion",
+      title: "Sample Title for First Article from Featured Articles",
+      author: "Kamaal Khan",
+      published_on: 1674298614,
+    },
+    {
+      image:
+        "https://demo.tagdiv.com/newspaper_pro/wp-content/uploads/2019/08/43-2-696x464.jpg",
+      cat: "fashion",
+      title: "Sample Title for First Article from Featured Articles",
+      author: "Kamaal Khan",
+      published_on: 1674298614,
+    },
+  ];
   return (
     <>
       <Grid
@@ -11,40 +45,19 @@ const ArticleFeatured = () => {
         rounded="md"
         w="full"
         templateRows="repeat(10, 1fr)"
-        templateColumns="repeat(8, 1fr)"
+        templateColumns="repeat(4, 1fr)"
       >
-        <GridItem
-          rowSpan={10}
-          colSpan={4}
-          backgroundSize="cover"
-          backgroundPosition="center"
-          backgroundRepeat="no-repeat"
-          backgroundImage="url('https://demo.tagdiv.com/newspaper_pro/wp-content/uploads/2019/08/h104-696x464.jpg')"
-        ></GridItem>
-        <GridItem
-          rowSpan={6}
-          colSpan={4}
-          backgroundSize="cover"
-          backgroundPosition="center"
-          backgroundRepeat="no-repeat"
-          backgroundImage="url('https://demo.tagdiv.com/newspaper_pro/wp-content/uploads/2019/08/28-696x368.jpg')"
-        ></GridItem>
-        <GridItem
-          rowSpan={4}
-          colSpan={2}
-          backgroundSize="cover"
-          backgroundPosition="center"
-          backgroundRepeat="no-repeat"
-          backgroundImage="url('https://demo.tagdiv.com/newspaper_pro/wp-content/uploads/2019/08/54-1-696x405.jpg')"
-        ></GridItem>
-        <GridItem
-          rowSpan={4}
-          colSpan={2}
-          backgroundSize="cover"
-          backgroundPosition="center"
-          backgroundRepeat="no-repeat"
-          backgroundImage="url('https://demo.tagdiv.com/newspaper_pro/wp-content/uploads/2019/08/43-2-696x464.jpg')"
-        ></GridItem>
+        {featured.map((item, i) => (
+          <GridItem
+            key={i}
+            rowSpan={i == 0 ? 10 : i == 1 ? 6 : i > 1 ? 4 : 4}
+            colSpan={i == 0 ? 2 : i == 1 ? 2 : i > 1 ? 1 : 1}
+            backgroundSize="cover"
+            backgroundPosition="center"
+            backgroundRepeat="no-repeat"
+            backgroundImage={`url('${item.image}')`}
+          ></GridItem>
+        ))}
       </Grid>
     </>
   );
